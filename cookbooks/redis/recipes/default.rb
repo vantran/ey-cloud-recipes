a25ed32994b8f98016326ef284a9b7c55aac8372
Van Tran <van@lixibox.com>
2015-09-04 12:46:01 +0700

    Install redis 2.6 on staging server

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -3,78 +3,76 @@
 # Recipe:: default
 #
 
-if ['util'].include?(node[:instance_role])
-  if node[:name] == 'utility'
+# if ['util'].include?(node[:instance_role])
+if ['app_master', 'app', 'solo', 'util'].include?(node[:instance_role])
+  sysctl "Enable Overcommit Memory" do
+    variables 'vm.overcommit_memory' => 1
+  end
 
-    sysctl "Enable Overcommit Memory" do
-      variables 'vm.overcommit_memory' => 1
-    end
+  enable_package "dev-db/redis" do
+    version node[:redis][:version]
+    override_hardmask true
+    unmask :true
+  end
 
-    enable_package "dev-db/redis" do
-      version node[:redis][:version]
-      override_hardmask true
-      unmask :true
-    end
+  package "dev-db/redis" do
+    version node[:redis][:version]
+    action :upgrade
+  end
 
-    package "dev-db/redis" do
-      version node[:redis][:version]
-      action :upgrade
-    end
+  directory "#{node[:redis][:basedir]}" do
+    owner 'redis'
+    group 'redis'
+    mode 0755
+    recursive true
+    action :create
+  end
 
-    directory "#{node[:redis][:basedir]}" do
-      owner 'redis'
-      group 'redis'
-      mode 0755
-      recursive true
-      action :create
-    end
+  template "/etc/redis_util.conf" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source "redis.conf.erb"
+    variables({
+      :pidfile => node[:redis][:pidfile],
+      :basedir => node[:redis][:basedir],
+      :basename => node[:redis][:basename],
+      :logfile => node[:redis][:logfile],
+      :loglevel => node[:redis][:loglevel],
+      :port  => node[:redis][:bindport],
+      :unixsocket => node[:redis][:unixsocket],
+      :saveperiod => node[:redis][:saveperiod],
+      :timeout => node[:redis][:timeout],
+      :databases => node[:redis][:databases],
+      :rdbcompression => node[:redis][:rdbcompression],
+      :hz => node[:redis][:hz]
+    })
+  end
 
-    template "/etc/redis_util.conf" do
-      owner 'root'
-      group 'root'
-      mode 0644
-      source "redis.conf.erb"
-      variables({
-        :pidfile => node[:redis][:pidfile],
-        :basedir => node[:redis][:basedir],
-        :basename => node[:redis][:basename],
-        :logfile => node[:redis][:logfile],
-        :loglevel => node[:redis][:loglevel],
-        :port  => node[:redis][:bindport],
-        :unixsocket => node[:redis][:unixsocket],
-        :saveperiod => node[:redis][:saveperiod],
-        :timeout => node[:redis][:timeout],
-        :databases => node[:redis][:databases],
-        :rdbcompression => node[:redis][:rdbcompression],
-        :hz => node[:redis][:hz]
-      })
-    end
-    
-    # redis-server is in /usr/bin on stable-v2, /usr/sbin for stable-v4
-    if Chef::VERSION[/^0.6/]
-      bin_path = "/usr/bin/redis-server"
-    else
-      bin_path = "/usr/sbin/redis-server"
-    end  
+  # redis-server is in /usr/bin on stable-v2, /usr/sbin for stable-v4
+  if Chef::VERSION[/^0.6/]
+    bin_path = "/usr/bin/redis-server"
+  else
+    bin_path = "/usr/sbin/redis-server"
+  end
 
-    template "/data/monit.d/redis_util.monitrc" do
-      owner 'root'
-      group 'root'
-      mode 0644
-      source "redis.monitrc.erb"
-      variables({
-        :profile => '1',
-        :configfile => '/etc/redis_util.conf',
-        :pidfile => node[:redis][:pidfile],
-        :logfile => node[:redis][:basename],
-        :port => node[:redis][:bindport],
-        :bin_path => bin_path
-      })
-    end
+  template "/data/monit.d/redis_util.monitrc" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source "redis.monitrc.erb"
+    variables({
+      :profile => '1',
+      :configfile => '/etc/redis_util.conf',
+      :pidfile => node[:redis][:pidfile],
+      :logfile => node[:redis][:basename],
+      :port => node[:redis][:bindport],
+      :bin_path => bin_path
+    })
+  end
 
-    execute "monit reload" do
-      action :run
-    end
+  execute "monit reload" do
+    action :run
   end
 end
 
