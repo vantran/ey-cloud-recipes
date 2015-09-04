e42850f98ff8b48347a88a52836ba5f9277e6200
Van Tran <van@lixibox.com>
2015-09-04 11:43:35 +0700

    Change redis-yml for staging env

--- cookbooks/redis-yml/recipes/default.rb
+++ cookbooks/redis-yml/recipes/default.rb
@@ -4,21 +4,20 @@ if ['app_master', 'app', 'solo', 'util'].include?(node[:instance_role])
   # redis_instance = node['utility_instances'].first
   # Otherwise, if you have multiple utility instances you can specify it by uncommenting the line below
   # You can change the name of the instance based on whatever name you have chosen for your instance.
-  redis_instance = node['utility_instances'].find { |instance| instance['name'] == 'utility' }
-  
-  if redis_instance
-    node[:applications].each do |app, data|
-      template "/data/#{app}/shared/config/redis.yml"do
-        source 'redis.yml.erb'
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0655
-        backup 0
-        variables({
-          :environment => node[:environment][:framework_env],
-          :hostname => redis_instance[:hostname]
-        })
-      end
+  # redis_instance = node['utility_instances'].find { |instance| instance['name'] == 'utility' }
+
+  # on staging we only have 1 server
+  node[:applications].each do |app, data|
+    template "/data/#{app}/shared/config/redis.yml" do
+      source 'redis.yml.erb'
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0655
+      backup 0
+      variables({
+        :environment => node[:environment][:framework_env],
+        :hostname => node[:engineyard][:environment][:instances].first[:private_hostname]
+      })
     end
   end
 end
