b3feb7b6556858ece2483e30aa9ca5e735655269
Van Tran <vantran53@gmail.com>
2016-06-30 17:46:53 +0700

    Chef recipe for Rpush

---
+++ cookbooks/rpush/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: rpush
+# Recipe:: default
+#
+if ['solo', 'util'].include?(node[:instance_role])
+  node[:applications].each do |app, data|
+    template "/etc/monit.d/rpush_#{app}.monitrc" do
+      owner 'root'
+      group 'root'
+      mode 0644
+      source "rpush.monitrc.erb"
+      variables({
+        :app_name => app
+      })
+    end
+
+    remote_file "/data/#{app}/shared/bin/rpush" do
+      source "rpush"
+      owner 'root'
+      group 'root'
+      mode 0755
+      backup 0
+    end
+  end
+
+  execute "ensure-rpush-is-setup-with-monit" do
+    command %Q{
+    monit reload
+    }
+  end
+end
